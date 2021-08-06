49cc809fa215098226bb802c7761366c67aa280b
SabaDodashvili <sabadodashvili777@gmail.com>
2021-08-07 01:27:23 +0400

    fixed many bugs

--- script.js
+++ script.js
@@ -1,44 +1,73 @@
 window.onload = function () {
   const input = document.querySelector('.calc__input');
   let calcString = '';
+  let repeatingOperator = true;
 
   document.querySelector('.calc').addEventListener('click', calcActions);
   function calcActions(e) {
     const targetElement = e.target;
-    if (targetElement.textContent !== '=' && targetElement.textContent !== 'c' && targetElement.textContent !== '⟵' && targetElement.closest('.calc__btn')) {
-      input.value += targetElement.textContent;
+    if (targetElement.closest('.calc__btn')) {
+      let lastSymbol = input.value[input.value.length - 1];
 
-      if (targetElement.textContent === '÷') {
+      let notNumbers = lastSymbol === '÷' || lastSymbol === '×' || lastSymbol === '%' || lastSymbol === '+' || lastSymbol === '-' || lastSymbol === '.';
+      let currentIsOperator =
+        targetElement.textContent === '÷' ||
+        targetElement.textContent === '×' ||
+        targetElement.textContent === '%' ||
+        targetElement.textContent === '+' ||
+        targetElement.textContent === '-' ||
+        targetElement.textContent === '.' ||
+        targetElement.textContent === '=' ||
+        targetElement.textContent === 'c' ||
+        targetElement.textContent === '⟵';
+
+      if (targetElement.textContent === '÷' && !notNumbers) {
         calcString = calcString + '/';
-      } else if (targetElement.textContent === '×') {
+        input.value += targetElement.textContent;
+      } else if (targetElement.textContent === '×' && !notNumbers) {
         calcString = calcString + '*';
-      } else if (targetElement.textContent === '%') {
+        input.value += targetElement.textContent;
+      } else if (targetElement.textContent === '%' && !notNumbers) {
         calcString = calcString + '/ 100 *';
-      } else {
+        input.value += targetElement.textContent;
+      } else if (targetElement.textContent === '+' && !notNumbers) {
+        calcString = calcString + '+';
+        input.value += targetElement.textContent;
+      } else if (targetElement.textContent === '-' && !notNumbers) {
+        calcString = calcString + '-';
+        input.value += targetElement.textContent;
+      } else if (targetElement.textContent === '.' && !notNumbers) {
+        calcString = calcString + '.';
+        input.value += targetElement.textContent;
+      } else if (targetElement.textContent === '=' && input.value.length > 0) {
+        input.value = eval(calcString);
+        calcString = String(eval(calcString));
+        return;
+      } else if (targetElement.textContent === 'c' && input.value.length > 0) {
+        input.value = '';
+        calcString = '';
+        return;
+      } else if (targetElement.textContent === '⟵' && input.value.length > 0) {
+        if (lastSymbol === '%') {
+          calcString = calcString.slice(0, calcString.length - 7);
+          console.log(1);
+        } else {
+          calcString = calcString.slice(0, -1);
+        }
+        input.value = input.value.slice(0, -1);
+        return;
+      } else if (!currentIsOperator) {
         calcString = calcString + targetElement.textContent;
+        input.value += targetElement.textContent;
       }
-    } else if (targetElement.textContent === '=' && input.value.length > 0) {
-      input.value = eval(calcString);
-      calcString = String(eval(calcString));
-    } else if (targetElement.textContent === 'c' && input.value.length > 0) {
-      input.value = '';
-      calcString = '';
-    } else if (targetElement.textContent === '⟵' && input.value.length > 0) {
-      if (input.value[input.value.length - 1] === '%') {
-        calcString = calcString.slice(0, calcString.length - 7);
-      } else {
-        calcString = calcString.slice(0, -1);
-      }
-      input.value = input.value.slice(0, -1);
     }
   }
 };
 
-//vanilla-tilt
-
+// vanilla - tilt;
 VanillaTilt.init(document.querySelectorAll('.vanilla'), {
-  max: 25,
+  max: 5,
   speed: 400,
   glare: true,
-  'max-glare': 0.2,
+  'max-glare': 0.1,
 });
